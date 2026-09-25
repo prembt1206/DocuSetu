0238e1375d11b8838258ecfc2bfae924ee64f932
prembt1206 <btprem166@gmail.com>
2026-09-25 21:13:36 +0530

    test: ESM export default handler

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
-module.exports = (req, res) => {
-  res.status(200).json({ hello: "world", url: req.url, method: req.method });
-};
+export default function handler(req, res) {
+  res.status(200).json({ hello: "world", time: new Date().toISOString() });
+}
